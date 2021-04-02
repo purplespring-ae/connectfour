4a8a4e96f6fc45a12acfea6a4b0f1d2c42323f52
unknown <alexander.evans@gmx.com>
2021-04-02 09:14:48 +0100

    Added opposite win vector checking

--- connect.py
+++ connect.py
@@ -33,7 +33,7 @@ def play_game():
             if verbose:
                 print(f"Column {col} doesn't exist on the board.")
             return False
-        elif not col.count(blank_spot) == 0:
+        elif list_col(col, True).count(blank_spot) == 0:
             if verbose:
                 print(f"Column {col} is full.")
             return False
@@ -48,9 +48,10 @@ def play_game():
         else:
             return True
 
-    def list_col(icol):
-        if not validate_col(icol):
-            return False
+    def list_col(icol, valid=False):
+        if not valid:
+            if not validate_col(icol):
+                return False
         result = []
         for r in range(len(board)):
             result.append(board[r][icol])
@@ -75,55 +76,65 @@ def play_game():
         print(f"It's {players[new_player]}'s turn.")
         return new_player
 
-    def check_win(r, c):
-    '''
-    ? only check the most recently added spot, as there can't be a win from
-    the start and no spots can move.
+    def check_win(check_r, check_c):
+        '''
+        ? only check the most recently added spot, as there can't be a win from
+        the start and no spots can move.
 
-    once game is fully running: allow AI to see three-in-a-rows and block them. first stage
-    of a rudimentary computer player
-    '''
-    possible_directions = {
-        "NW": {"delta_r": -1, "delta_c": -1},
-        "N": {"delta_r": -1, "delta_c": 0},
-        "NE": {"delta_r": -1, "delta_c": +1},
-        "E": {"delta_r": 0, "delta_c": +1},
-        "SE": {"delta_r": +1, "delta_c": +1},
-        "S": {"delta_r": +1, "delta_c": 0},
-        "SW": {"delta_r": +1, "delta_c": -1},
-        "W": {"delta_r": 0, "delta_c": +1},
-    }
+        once game is fully running: allow AI to see three-in-a-rows and block them, as first stage
+        of a rudimentary computer player
+        '''
+        possible_directions = {
+            "NW": {"delta_r": -1, "delta_c": -1, "opp": "SE"},
+            "N": {"delta_r": -1, "delta_c": 0, "opp": "S"},
+            "NE": {"delta_r": -1, "delta_c": +1, "opp": "SW"},
+            "E": {"delta_r": 0, "delta_c": +1, "opp": "W"},
+            "SE": {"delta_r": +1, "delta_c": +1, "opp": "NW"},
+            "S": {"delta_r": +1, "delta_c": 0, "opp": "N"},
+            "SW": {"delta_r": +1, "delta_c": -1, "opp": "NE"},
+            "W": {"delta_r": 0, "delta_c": +1, "opp": "E"},
+        }
+        checked_directions = []
 
-    # start_from should be tuple in the format (r,c). add validation if it causes issues
-    check_r, check_c = r, c
-    match_val = board[check_r][check_c]
+        def check_direction(from_r, from_c, dir_):
+            # set up vector and choose first target
+            delta_r = possible_directions[dir_]["delta_r"]
+            delta_c = possible_directions[dir_]["delta_c"]
+            target_r, target_c = from_r + delta_r, check_c + delta_c
+            # loop-breaking vars
+            match_val = board[from_r][from_c]
+            dir_exhausted = False
+            num_connect = 0
 
-    for dir, vals in possible_directions.items():
+            while not dir_exhausted and num_connect < 3:
+                if not validate_row(target_r) \
+                        or not validate_col(target_c) \
+                        or not board[target_r][target_c] == match_val:
+                    # print(f"{from_r},{from_c} has no matches to the {dir}."")
+                    dir_exhausted = True
+                    break
+                else:
+                    print(
+                        f"{target_r},{target_c} is a connect to the {dir_} for {from_r},{from_c}.")
+                    num_connect += 1
+                    # adjust position to continue in that direction
+                    target_r, target_c = target_r + delta_r, target_c + delta_c
+            return num_connect
 
-        delta_r = possible_directions[dir]["delta_r"]
-        delta_c = possible_directions[dir]["delta_c"]
-        target_r, target_c = check_r + delta_r, check_c + delta_c
-        dir_exhausted = False
-        num_connect = 1
-
-        while not dir_exhausted and num_connect < 4:
-            if not validate_row(target_r) or not validate_col(target_c) or not board[target_r][target_c] == match_val:
-                # print(f"{start_from} has no neighbours to the {dir}.")
-                dir_exhausted = True
+        for dir, vals in possible_directions.items():
+            if dir in checked_directions:
+                continue
+            opp_dir = possible_directions[dir]["opp"]
+            dir_match = check_direction(check_r, check_c, dir)
+            checked_directions.append(dir)
+            opp_match = check_direction(check_r, check_c, opp_dir)
+            checked_directions.append(opp_dir)
+            if dir_match + opp_match >= 3:
+                print(
+                    "SOMEONE WON THE GAME I DON'T KNOW WHO YET OR WHAT TO DO ABOUT IT THOUGH")
                 break
             else:
-                print(
-                    f"{target_r},{target_c} is a connect to the {dir} for {check_r},{check_c}.")
-                num_connect += 1
-                # adjust position to continue in that direction
-                target_r, target_c = target_r + delta_r, target_c + delta_c
-            continue
-        if num_connect == 4:
-            print(
-                "SOMEONE WON THE GAME I DON'T KNOW WHO YET OR WHAT TO DO ABOUT IT THOUGH")
-
-        # TODO: currently detects connects all going in the same compass direction -
-        # need to combine opposite directions to properly check for win
+                pass
 
     blank_spot = " . "
     players = [" X ", " O "]
@@ -131,24 +142,24 @@ def play_game():
     board = construct_board(r=6, c=7, blank_spot=blank_spot)
     draw_board(board)
 
-    # TEST MOVES
-    make_move(5, current_player)
-    current_player = toggle_player(current_player)
-    make_move(2, current_player)
-    current_player = toggle_player(current_player)
-    make_move(5, current_player)
-    current_player = toggle_player(current_player)
-    make_move(3, current_player)
-    current_player = toggle_player(current_player)
-    make_move(5, current_player)
-    current_player = toggle_player(current_player)
-    make_move(2, current_player)
-    current_player = toggle_player(current_player)
-    make_move(5, current_player)
-    current_player = toggle_player(current_player)
-    make_move(2, current_player)
-    current_player = toggle_player(current_player)
-    check_win(5, 5)
+    # # TEST MOVES
+    # make_move(5, current_player)
+    # current_player = toggle_player(current_player)
+    # make_move(2, current_player)
+    # current_player = toggle_player(current_player)
+    # make_move(5, current_player)
+    # current_player = toggle_player(current_player)
+    # make_move(3, current_player)
+    # current_player = toggle_player(current_player)
+    # make_move(5, current_player)
+    # current_player = toggle_player(current_player)
+    # make_move(2, current_player)
+    # current_player = toggle_player(current_player)
+    # make_move(5, current_player)
+    # current_player = toggle_player(current_player)
+    # make_move(2, current_player)
+    # current_player = toggle_player(current_player)
+    # check_win(4, 5)
 
 
 if __name__ == "__main__":
